26a68cb8c12b6258c1359d67ce74207c05cda6e8
Anthony Liu <anthonyliu@brandeis.edu>
2021-02-12 11:59:11 -0800

    first commit

---
+++ EthernetTest/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿using System;
+
+namespace EthernetTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectButton1 = new System.Windows.Forms.Button();
+            this.ethernetOn1 = new System.Windows.Forms.Label();
+            this.dirLabel = new System.Windows.Forms.Label();
+            this.changeDir = new System.Windows.Forms.Button();
+            this.currentDir = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.connectButton2 = new System.Windows.Forms.Button();
+            this.ethernetOn2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // connectButton1
+            // 
+            this.connectButton1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.connectButton1.FlatAppearance.BorderSize = 0;
+            this.connectButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectButton1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.connectButton1.Location = new System.Drawing.Point(29, 27);
+            this.connectButton1.Name = "connectButton1";
+            this.connectButton1.Size = new System.Drawing.Size(138, 30);
+            this.connectButton1.TabIndex = 3;
+            this.connectButton1.Text = "Ethernet Port 1";
+            this.connectButton1.UseVisualStyleBackColor = false;
+            this.connectButton1.Click += new System.EventHandler(this.connectButton1_Click);
+            // 
+            // ethernetOn1
+            // 
+            this.ethernetOn1.AutoSize = true;
+            this.ethernetOn1.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ethernetOn1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ethernetOn1.Location = new System.Drawing.Point(30, 75);
+            this.ethernetOn1.Name = "ethernetOn1";
+            this.ethernetOn1.Size = new System.Drawing.Size(86, 18);
+            this.ethernetOn1.TabIndex = 6;
+            this.ethernetOn1.Text = "connected";
+            // 
+            // dirLabel
+            // 
+            this.dirLabel.AutoSize = true;
+            this.dirLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dirLabel.ForeColor = System.Drawing.Color.White;
+            this.dirLabel.Location = new System.Drawing.Point(10, 125);
+            this.dirLabel.Name = "dirLabel";
+            this.dirLabel.Size = new System.Drawing.Size(75, 18);
+            this.dirLabel.TabIndex = 11;
+            this.dirLabel.Text = "Directory:";
+            // 
+            // changeDir
+            // 
+            this.changeDir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeDir.Location = new System.Drawing.Point(17, 255);
+            this.changeDir.Name = "changeDir";
+            this.changeDir.Size = new System.Drawing.Size(90, 30);
+            this.changeDir.TabIndex = 12;
+            this.changeDir.Text = "Assign";
+            this.changeDir.UseVisualStyleBackColor = true;
+            this.changeDir.Click += new System.EventHandler(this.changeDir_Click);
+            // 
+            // currentDir
+            // 
+            this.currentDir.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.currentDir.AutoEllipsis = true;
+            this.currentDir.AutoSize = true;
+            this.currentDir.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentDir.ForeColor = System.Drawing.Color.White;
+            this.currentDir.Location = new System.Drawing.Point(10, 150);
+            this.currentDir.MaximumSize = new System.Drawing.Size(160, 80);
+            this.currentDir.Name = "currentDir";
+            this.currentDir.Size = new System.Drawing.Size(0, 17);
+            this.currentDir.TabIndex = 13;
+            this.currentDir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(18, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 18);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "**TURN OFF WIFI BEFORE RUNNING**";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.dirLabel);
+            this.panel1.Controls.Add(this.currentDir);
+            this.panel1.Controls.Add(this.changeDir);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 305);
+            this.panel1.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(10, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 81);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Ethernet\r\nDiagnositic\r\nTool";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ethernetOn2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.connectButton2);
+            this.panel2.Controls.Add(this.connectButton1);
+            this.panel2.Controls.Add(this.ethernetOn1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(180, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(340, 305);
+            this.panel2.TabIndex = 16;
+            // 
+            // connectButton2
+            // 
+            this.connectButton2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.connectButton2.FlatAppearance.BorderSize = 0;
+            this.connectButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectButton2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.connectButton2.Location = new System.Drawing.Point(29, 125);
+            this.connectButton2.Name = "connectButton2";
+            this.connectButton2.Size = new System.Drawing.Size(138, 30);
+            this.connectButton2.TabIndex = 9;
+            this.connectButton2.Text = "Ethernet Port 2";
+            this.connectButton2.UseVisualStyleBackColor = false;
+            this.connectButton2.Click += new System.EventHandler(this.connectButton2_Click);
+            // 
+            // ethernetOn2
+            // 
+            this.ethernetOn2.AutoSize = true;
+            this.ethernetOn2.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ethernetOn2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ethernetOn2.Location = new System.Drawing.Point(30, 175);
+            this.ethernetOn2.Name = "ethernetOn2";
+            this.ethernetOn2.Size = new System.Drawing.Size(86, 18);
+            this.ethernetOn2.TabIndex = 15;
+            this.ethernetOn2.Text = "connected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 305);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Name = "Form1";
+            this.Text = "AQUABEAM® Ethernet Port Test";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button connectButton1;
+        private System.Windows.Forms.Label ethernetOn1;
+        private System.Windows.Forms.Label dirLabel;
+        private System.Windows.Forms.Button changeDir;
+        private System.Windows.Forms.Label currentDir;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button connectButton2;
+        private System.Windows.Forms.Label ethernetOn2;
+    }
+}
+
---
+++ EthernetTest/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace EthernetTest
+{
+    public partial class Form1 : Form
+    {
+        static string init;
+        static string folderPath = "";
+        static string absFilePath;
+        static string raw_date;
+        string connectStatusPort1 = string.Empty;
+        string connectStatusPort2 = string.Empty;
+
+
+        [STAThread]
+        public static void Main()
+        {
+            //begin application
+            Application.EnableVisualStyles();
+            Application.Run(new Form1());
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            raw_date = DateTime.Now.ToString()+"/r/n";
+            connectButton1.Enabled = false;
+            connectButton2.Enabled = false;
+        }
+
+        //redundant code
+        private void connectButton1_Click(object sender, EventArgs e)
+        {
+            string s = "192.168.1.120";
+            string output = Ping(s);
+            ethernetOn1.Text = "";
+            connectStatusPort1 = "";
+            if (output.Length > 60)
+            {
+                ethernetOn1.Text = "Ethernet is connected";
+                ethernetOn1.ForeColor = System.Drawing.Color.ForestGreen;
+            }
+            else if (output == "DestinationHostUnreachable" || output == "TimedOut")
+            {
+                output = "Address: " + s + "\r\nError: " + output;
+                ethernetOn1.Text = "Ethernet is not connected";
+                ethernetOn1.ForeColor = System.Drawing.Color.Red;
+            }
+            connectStatusPort1 = output;
+            init += "Port 1: "+output+"\r\n";
+        }
+
+        private void connectButton2_Click(object sender, EventArgs e)
+        {
+            string s = "192.168.1.122";
+            string output = Ping(s);
+            ethernetOn2.Text = "";
+            connectStatusPort2 = "";
+            if (output.Length > 60)
+            {
+                ethernetOn2.Text = "Ethernet is connected";
+                ethernetOn2.ForeColor = System.Drawing.Color.ForestGreen;
+            }
+            else if (output == "DestinationHostUnreachable" || output == "TimedOut")
+            {
+                output = "Address: " + s + "\r\nError: " + output;
+                ethernetOn2.Text = "Ethernet is not connected";
+                ethernetOn2.ForeColor = System.Drawing.Color.Red;
+            }
+            connectStatusPort2 = output;
+            init += "Port 2: " + output + "\r\n";
+        }
+
+        private void checkIfWifiConnected(Button b)
+        {
+           string s = string.Empty;
+           foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.OperationalStatus == OperationalStatus.Up)
+                {
+                    Console.WriteLine(ni.NetworkInterfaceType.ToString());
+                    s = Regex.Replace(ni.NetworkInterfaceType.ToString(), @"[\d-]", string.Empty);
+                    switch (s)
+                    {
+                        case "Wireless":
+                            label1.Text = "Please disable your wifi.";
+                            b.Enabled = false;
+                            break;
+                        case "Ethernet":
+                            label1.Text = "Ethernet is connected";
+                            b.Enabled = true;
+                            continue;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        //ping the ip address provided through the ethernet port
+        private string Ping(string input)
+        {
+            Ping pingSender = new Ping();
+            //check for invalid input
+            if (input.Count(c => c == '.') != 3)
+            {
+                if (input == null || input == "")
+                {
+                    return ("Error: No IP address provided.");
+                }
+                return ("Error: An invalid IP was entered.");
+            }
+            try
+            {
+                IPAddress address = IPAddress.Parse(input);
+                PingReply reply = pingSender.Send(address, 5);
+                if (reply.Status == IPStatus.Success)
+                {
+                    string addr = "Address: " + reply.Address.ToString();
+                    string rtt = "RoundTrip time: " + reply.RoundtripTime;
+                    string ttl = "Time to live: " + reply.Options.Ttl;
+                    string bs = "Buffer size: " + reply.Buffer.Length;
+                    return addr + "\r\n" + rtt + "\r\n" + ttl  + "\r\n" + bs + "\r\n";
+                }
+                else
+                {
+                    return reply.Status.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                if (e is PingException)
+                {
+                    return ("Error: Please re-enter IP");
+                }
+                else if (e is FormatException || e is ArgumentException)
+                {
+                    return ("Error: An invalid IP was entered.");
+                }
+                throw;
+            }
+        }
+
+        //uncomment to add message after log
+        static private void OnProcessExit(object sender, EventArgs e)
+        {
+            if (folderPath != "")
+            {
+                using (StreamWriter file = new StreamWriter(absFilePath))
+                {
+                    file.WriteLine(init);
+                    file.WriteLine("Test Terminated: " + DateTime.Now.ToString());
+                    file.Dispose();
+                }
+            }
+           
+        }
+
+        static private void InitPath()
+        {
+            //grab the system date to create log
+            string raw_date = DateTime.Now.ToString();
+            string filename = "log_" + raw_date.Replace(" ", "_").Replace(":", ".").Replace("/", ".") + ".txt";
+            init += "Device: " + Environment.MachineName + "\r\nTest Initialized: " + raw_date + "\r\n";
+            absFilePath = Path.Combine(folderPath, filename);
+        }
+
+        private void changeDir_Click(object sender, EventArgs e)
+        {
+            DialogResult fileBrowse = folderBrowserDialog1.ShowDialog();
+            if (fileBrowse == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+                currentDir.Text = folderPath;
+                InitPath();
+                changeDir.Text = "Change";
+                changeDir.Update();
+                //uncomment below to test wifi
+                connectButton1.Enabled = true;
+                connectButton2.Enabled = true;
+            }
+        }
+
+        private void ipInputField1_Click(object sender, EventArgs e)
+        {
+            checkIfWifiConnected(connectButton1);
+        }
+
+        private void ipInputField2_Click(object sender, EventArgs e)
+        {
+            checkIfWifiConnected(connectButton2);
+        }
+    }
+}
